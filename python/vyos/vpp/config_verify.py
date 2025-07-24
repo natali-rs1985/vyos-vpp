3832629fe7a57de3d202253619c2f4ce3826985f
Nataliia Solomko <natalirs1985@gmail.com>
2025-07-24 16:38:26 +0300

    T7658: Increase max-map-count interval and default value to linux default
    Also input a warning instead of ConfigError in case of "host-resources max-map-count" didn't pass the verification

--- python/vyos/vpp/config_verify.py
+++ python/vyos/vpp/config_verify.py
@@ -19,6 +19,7 @@
 import psutil
 
 from vyos import ConfigError
+from vyos.base import Warning
 from vyos.utils.cpu import get_core_count as total_core_count
 
 from vyos.vpp.control_host import get_eth_driver
@@ -398,6 +399,6 @@ def verify_vpp_host_resources(config: dict):
     hugepages = mem_checks.get_hugepages_total()
 
     if max_map_count < 2 * hugepages:
-        raise ConfigError(
+        Warning(
             'The max_map_count must be greater than or equal to (2 * HugePages_Total)'
         )
